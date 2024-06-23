fcb20338063ee0bc759f9038802eb4bb364db11c
Sheraz <31206569+sheraz24@users.noreply.github.com>
2024-06-23 15:04:41 +0200

    Files from current site on 23.06.2024

---
+++ lang/de.js
@@ -0,0 +1,37 @@
+/*
+
+	this is a language file
+	it is the german language file
+
+*/
+var lang = lang || {};
+lang['de'] = {};
+
+lang['de']['of']    = "von";
+lang['de']['yes']   = "Stimme zu";
+lang['de']['no']    = "Stimme nicht zu";
+lang['de']['maybe'] = "Neutral";
+lang['de']['skip']  = "Überspringen";
+lang['de']['back']  = "Zurück";
+
+lang['de']['switchLangShort'] = 'switch to english'
+lang['de']['switchLanguage'] = "Changing the language will result in loosing all answers given to this point. Proceed?"
+
+lang['de']['logoClick'] = "Wenn Sie die Seite neu laden verlieren Sie alle Angaben, die sie bisher gemacht haben\nReloading the page will cause loss of all answers given. Proceed?"
+
+// first page
+lang['de']['header'] = "Europawahl 2024";
+lang['de']['js'] = '<strong>Ohne Java-Script geht hier leider nichts. Dafür berechnen wir auch alles nur auf deinem Gerät und senden deine Meinungen an keinen Server.<br />Deal?</strong>';
+
+lang['de']['einleitung'] = '<p>Ihre Antworten werden nur auf Ihrem Gerät (Computer, Tablet, Smartphone, etc.) ausgewertet und an keinen Server gesendet. Das heißt aber auch, dass Ihre Antworten verloren gehen, sobald Sie diese Seite neu laden oder verlassen.</p>';
+
+lang['de']['losgehts'] = "Los geht's!";
+
+// second page
+lang['de']['weighting'] = 'Welche Positionen sind Ihnen besonders wichtig?';
+lang['de']['gtxt'] = '<p>Von Ihnen markierte Positionen werden bei der Auswertung doppelt gezählt und in Ihrer Ergebnistabelle farbig hinterlegt.</p>';
+lang['de']['next'] = 'Weiter zur Auswertung';
+
+// third page
+lang['de']['result'] = '<p>Der Science-O-Mat zur Europawahl 2024 ergab für Sie untenstehendes Ergebnis bzgl. der prozentualen Übereinstimmung der Antworten auf die Positionen. Das Ergebnis beinhaltet keine Wahlempfehlung, sondern ist ein auf die Themenpalette der Scientists for Future fokussiertes Informationsangebot, das die Positionen der zur Wahl antretenden Parteien und sonstigen politischen Vereinigungen zu den 15 ihnen vorgelegten Positionen transparent macht. Das Projekt ist kein Bestandteil des Wahl-O-Mat der Bundeszentrale für politische Bildung.</p><p>In der Tabelle sind die Parteien und sonstigen politischen Vereinigungen in der Reihenfolge gelistet, die sich aus Ihrem persönlichen Übereinstimmungsergebnis ergibt.</p><p>HINWEIS: Mit einem Klick auf die Tabellenzeilen mit den jeweiligen Positionen können Sie sich jeweils eine Liste der Stellungnahmen aller Parteien und sonstigen politischen Vereinigungen anzeigen lassen.</p>';
+lang['de']['furtherinfos'] = '<p style="font-size: 9pt;">*Aus technischen Gründen haben wir den Namen "Partei für schulmedizinische Verjüngungsforschung" hier zu Verjüngungsforschung abgekürzt. Dies ist keine offizielle Kurzform.</p><br><br><div style="display:flex;align-items:center; margin-left:-10px"><div class="w_pro legend"></div><p>Stimme zu</p><div class="w_neutral legend"></div><p>Neutral</p><div class="w_against legend"></div><p>Stimme nicht zu</p></div><p style="margin-bottom:50px;">Von Ihnen doppelt gewichtete Positionen sind <span style="background: #f2ae8b; padding: 5px;">farbig hervorgehoben.</span></p><p style="margin-bottom:50px;">Erläuterungen zur Vorgehensweise und Berechnung der Ergebnisse finden Sie auf der Startseite: <a href="../#section03" target="_blank" rel="noopener noreferrer">Link</a>.</p><p style="margin-bottom:50px;">Hat Ihnen der Science-O-Mat gefallen? Dann teilen Sie den <a href="../" target="_blank" rel="noopener noreferrer">Link</a> in den Sozialen Medien und im Bekanntenkreis!</p>';
